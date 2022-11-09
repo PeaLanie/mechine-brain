4026c0eabeaade79771afc051abbbea9ff82ac74
PeaLanie <101152620+PeaLanie@users.noreply.github.com>
2022-11-09 14:00:24 +0200

    fresh start

--- app.js
+++ app.js
@@ -1,199 +1,84 @@
-// These are the two fuctions that open and close the sidebar ---------------------------
+$(function() {
+    $('.fa-bars').on('click', () => {
+        $('#side-nav').addClass('show-nav')
+        $('.fa-bars').hide()
+    })
+    $('.fa-xmark').on('click', () => {
+        $('#side-nav').removeClass('show-nav')
+        $('.fa-bars').show()
+    })
+    $('aside li').on('click', () => {
+        $('#side-nav').removeClass('show-nav')
+        $('.fa-bars').show()
+    })
+    let numbers_el = $('.the-list span')
+    let sum = 0
+    let grade_el = $('#grade')
+    numbers_el.each(function() {
+        let each_num
+        $(this).text(Math.floor(Math.random() * 9) + 1)
+        each_num = $(this).text()
+        sum += parseInt(each_num)
+    })
+    $('#submit').on('click', () => {
+        const input_el = $('#users-answer')
+        let users_answer = parseInt(input_el.val())
+        
+        if (users_answer === sum) {
+            if (grade_el.attr('class') == 'grade wrong') {
+                grade_el.removeClass('wrong')
+                grade_el.addClass('correct')
+                grade_el.text(sum+' is Correct.')
+            } else if (grade_el.attr('class') == 'grade empty') {
+                grade_el.removeClass('empty')
+                grade_el.addClass('correct')
+                grade_el.text(sum+' is Correct.')
+            } else {
+                grade_el.addClass('correct')
+                grade_el.text(sum+' is Correct.')
+            }
+        } else if (users_answer != sum && input_el.val() != '') {
+            if (grade_el.attr('class') == 'grade correct') {
+                grade_el.removeClass('correct')
+                grade_el.addClass('wrong')
+                grade_el.text(sum+', Try Again!')
+            } else if (grade_el.attr('class') == 'grade empty') {
+                grade_el.removeClass('empty')
+                grade_el.addClass('wrong')
+                grade_el.text(sum+', Try Again!')
+            } else {
+                grade_el.addClass('wrong')
+                grade_el.text(sum+', Try Again!')
+            }
+        } else if (input_el.val() == '') {
+            if (grade_el.attr('class') == 'grade wrong') {
+                grade_el.removeClass('wrong')
+                grade_el.addClass('empty')
+                grade_el.text('Write your answer below.')
+            } else if (grade_el.attr('class') == 'grade correct') {
+                grade_el.removeClass('correct')
+                grade_el.addClass('empty')
+                grade_el.text('Write your answer below.')
+            } else {
+                grade_el.addClass('empty')
+                grade_el.text('Write your answer below.')
+                input_el.css('border', )
+            }
+        }
+        input_el.val('')
+    })
+    $('#reload').on('click', () => {
+        sum = 0
+        if (grade_el.attr('class') == 'grade wrong' || grade_el.attr('class') == 'grade correct' || grade_el.attr('class') == 'grade empty') {
+            grade_el.attr('class', 'grade')
+            grade_el.text('No Results Yet')
+        }
+        numbers_el.each(function() {
+            let each_num
+            $(this).text(Math.floor(Math.random() * 9) + 1)
+            each_num = $(this).text()
+            sum += parseInt(each_num)
+        })
+    })
+});
 
-function openNav() {
-    document.getElementById("side-nav").style.display = "block";
-    document.getElementById("menu-icon").style.display = "none";
-}
-
-function closeNav() {
-    document.getElementById("side-nav").style.display = "none";
-    document.getElementById("menu-icon").style.display = "block";
-}
-
-// --------------------------- End --------------------------------
-
-
-function randomN(min, max, inc) {
-    return Math.floor(Math.random() * (max - min) / inc) * inc + min;
-}
-
-
-
-// These are the fuctions that display the numbers on the page -----------------------------
-
-function basic() {
-var randOne = Math.floor(Math.random() * 9) + 1;
-var randTwo = Math.floor(Math.random() * 9) + 1;
-var randThree = Math.floor(Math.random() * 9) + 1;
-var randFour = Math.floor(Math.random() * 9) + 1;
-var randFive = Math.floor(Math.random() * 9) + 1;
-
-var indexOne = document.getElementById('basic1');
-var indexTwo = document.getElementById('basic2');
-var indexThree = document.getElementById('basic3');
-var indexFour = document.getElementById('basic4');
-var indexFive = document.getElementById('basic5');
-
-indexOne.innerHTML = randOne;
-indexTwo.innerHTML = randTwo;
-indexThree.innerHTML = randThree;
-indexFour.innerHTML = randFour;
-indexFive.innerHTML = randFive;
-}
-basic();
-
-
-function beginner() {
-    var randOne = randomN(10, 90, 10);
-    var randTwo = randomN(10, 90, 10);
-    var randThree = randomN(10, 90, 10);
-    var randFour = randomN(10, 90, 10);
-    var randFive = randomN(10, 90, 10);
-    
-    var indexOne = document.getElementById('beginner1');
-    var indexTwo = document.getElementById('beginner2');
-    var indexThree = document.getElementById('beginner3');
-    var indexFour = document.getElementById('beginner4');
-    var indexFive = document.getElementById('beginner5');
-    
-    indexOne.innerHTML = randOne;
-    indexTwo.innerHTML = randTwo;
-    indexThree.innerHTML = randThree;
-    indexFour.innerHTML = randFour;
-    indexFive.innerHTML = randFive;
-}
-beginner();
-
-
-function intermediate() {
-    var randOne = Math.floor(Math.random() * 9) + 1;
-    var randTwo = Math.floor(Math.random() * 9) + 1;
-    var randThree = Math.floor(Math.random() * 9) + 1;
-    var randFour = Math.floor(Math.random() * 9) + 1;
-    var randFive = Math.floor(Math.random() * 9) + 1;
-    
-    var indexOne = document.getElementById('basic1');
-    var indexTwo = document.getElementById('basic2');
-    var indexThree = document.getElementById('basic3');
-    var indexFour = document.getElementById('basic4');
-    var indexFive = document.getElementById('basic5');
-    
-    indexOne.innerHTML = randOne;
-    indexTwo.innerHTML = randTwo;
-    indexThree.innerHTML = randThree;
-    indexFour.innerHTML = randFour;
-    indexFive.innerHTML = randFive;
-}
-
-
-function advanced() {
-    var randOne = Math.floor(Math.random() * 9) + 1;
-    var randTwo = Math.floor(Math.random() * 9) + 1;
-    var randThree = Math.floor(Math.random() * 9) + 1;
-    var randFour = Math.floor(Math.random() * 9) + 1;
-    var randFive = Math.floor(Math.random() * 9) + 1;
-    
-    var indexOne = document.getElementById('basic1');
-    var indexTwo = document.getElementById('basic2');
-    var indexThree = document.getElementById('basic3');
-    var indexFour = document.getElementById('basic4');
-    var indexFive = document.getElementById('basic5');
-    
-    indexOne.innerHTML = randOne;
-    indexTwo.innerHTML = randTwo;
-    indexThree.innerHTML = randThree;
-    indexFour.innerHTML = randFour;
-    indexFive.innerHTML = randFive;
-}
-
-
-
-function master() {
-    var randOne = Math.floor(Math.random() * 9) + 1;
-    var randTwo = Math.floor(Math.random() * 9) + 1;
-    var randThree = Math.floor(Math.random() * 9) + 1;
-    var randFour = Math.floor(Math.random() * 9) + 1;
-    var randFive = Math.floor(Math.random() * 9) + 1;
-    
-    var indexOne = document.getElementById('basic1');
-    var indexTwo = document.getElementById('basic2');
-    var indexThree = document.getElementById('basic3');
-    var indexFour = document.getElementById('basic4');
-    var indexFive = document.getElementById('basic5');
-    
-    indexOne.innerHTML = randOne;
-    indexTwo.innerHTML = randTwo;
-    indexThree.innerHTML = randThree;
-    indexFour.innerHTML = randFour;
-    indexFive.innerHTML = randFive;
-}
-
-
-function expert() {
-    var randOne = Math.floor(Math.random() * 9) + 1;
-    var randTwo = Math.floor(Math.random() * 9) + 1;
-    var randThree = Math.floor(Math.random() * 9) + 1;
-    var randFour = Math.floor(Math.random() * 9) + 1;
-    var randFive = Math.floor(Math.random() * 9) + 1;
-    
-    var indexOne = document.getElementById('basic1');
-    var indexTwo = document.getElementById('basic2');
-    var indexThree = document.getElementById('basic3');
-    var indexFour = document.getElementById('basic4');
-    var indexFive = document.getElementById('basic5');
-    
-    indexOne.innerHTML = randOne;
-    indexTwo.innerHTML = randTwo;
-    indexThree.innerHTML = randThree;
-    indexFour.innerHTML = randFour;
-    indexFive.innerHTML = randFive;
-}
-
-
-function next() {
-    var randOne = Math.floor(Math.random() * 9) + 1;
-    var randTwo = Math.floor(Math.random() * 9) + 1;
-    var randThree = Math.floor(Math.random() * 9) + 1;
-    var randFour = Math.floor(Math.random() * 9) + 1;
-    var randFive = Math.floor(Math.random() * 9) + 1;
-    
-    var indexOne = document.getElementById('basic1');
-    var indexTwo = document.getElementById('basic2');
-    var indexThree = document.getElementById('basic3');
-    var indexFour = document.getElementById('basic4');
-    var indexFive = document.getElementById('basic5');
-    
-    indexOne.innerHTML = randOne;
-    indexTwo.innerHTML = randTwo;
-    indexThree.innerHTML = randThree;
-    indexFour.innerHTML = randFour;
-    indexFive.innerHTML = randFive;
-}
-
-
-
-// End of function!! ---------------------------------------------------------------------
-
-
-// This is the fuction that adds the numbers and checks if the users input is correct ------------
-
-function checkAnswer(id1, id2, id3, id4, id5, outPut, usersAnswer) {
-    var indexOne = document.getElementById(id1).textContent;
-    var indexTwo = document.getElementById(id2).textContent;
-    var indexThree = document.getElementById(id3).textContent;
-    var indexFour = document.getElementById(id4).textContent;
-    var indexFive = document.getElementById(id5).textContent;
-
-
-    var sum = parseInt(indexOne) + parseInt(indexTwo) + parseInt(indexThree) + parseInt(indexFour) + parseInt(indexFive);
-    var answer = document.getElementById(usersAnswer).value;
-    if (parseInt(answer) == sum) {
-        document.getElementById(outPut).innerHTML = ('Correct! ' + '<span class="greenNum">' + sum.toString() + '</span>' + ' is the right answer. Click "Reload" for more practice!')
-    }
-    else if (parseInt(answer) != sum) {
-        outPut = document.getElementById(outPut);
-        outPut.innerHTML = ('<span class="oops">' + 'Oops!' + '</span>' + 'The correct answer is ' + '<span class="greenNum">' + sum.toString() + '</span>' + '. Please click "Reload" and try again. (Make sure you insert an answer;and it must be an Interger).')
-    }
-}
-
-// End -------------------------------------------------------------------------------------------------------------
